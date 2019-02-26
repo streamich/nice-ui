b67a3037c9380e7915ff97eaae6fa354d3b6a2d4
streamich <streamich@gmail.com>
2019-02-26 17:50:16 +0100

    style: 💄 run Prettier

--- src/markdown/renderers/renderSup.tsx
+++ src/markdown/renderers/renderSup.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import {RenderNode, ISup} from '../types';
 
-const renderSup: RenderNode = (renderers, node: ISup, props, state) =>
-  <sup>{renderers.children(renderers, node.children, props, state)}</sup>;
+const renderSup: RenderNode = (renderers, node: ISup, props, state) => (
+  <sup>{renderers.children(renderers, node.children, props, state)}</sup>
+);
 
 export default renderSup;
