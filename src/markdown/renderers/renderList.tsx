2248bb168d8302464f6574db705d7c9e38d42bba
streamich <streamich@gmail.com>
2019-03-06 22:54:00 +0100

    feat(markdown): 🎸 support ordered lists

--- src/markdown/renderers/renderList.tsx
+++ src/markdown/renderers/renderList.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import {RenderNode} from '../types';
 
 const renderList: RenderNode = (renderers, flat, idx, props, state) => {
-  return <ul>{renderers.children(renderers, flat, idx, props, state)}</ul>;
+  const node = flat.nodes[idx] as any;
+  const tag = node.ordered ? 'ol' : 'ul';
+  return React.createElement(tag, null, renderers.children(renderers, flat, idx, props, state));
 };
 
 export default renderList;
