38c6d8656f8f44a229121ca943026f63c0f92483
streamich <streamich@gmail.com>
2024-07-20 23:41:33 +0200

    style: 💄 run Prettier

--- src/4-card/Scrollbox/index.tsx
+++ src/4-card/Scrollbox/index.tsx
@@ -67,7 +67,12 @@ export const Scrollbox: React.FC<ScrollboxProps> = ({children, ...rest}) => {
 
   return (
     <div className={wrapClass}>
-      <div {...rest} ref={ref} className={(rest.className || '') + blockClass} style={{...rest.style, overflowY: shadows[0] || shadows[1] ? 'auto' : undefined,}}>
+      <div
+        {...rest}
+        ref={ref}
+        className={(rest.className || '') + blockClass}
+        style={{...rest.style, overflowY: shadows[0] || shadows[1] ? 'auto' : undefined}}
+      >
         {children}
       </div>
       <div className={shadowTopClass} style={{opacity: shadows[0] ? 1 : 0, background}} />
